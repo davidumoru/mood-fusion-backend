4765d9a49158d620a20ac7701018651d84ed1766
David Umoru <umorudavido@gmail.com>
2023-08-27 16:37:18 +0100

    Connect to database

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 
 const spotifyAuthRoutes = require('./routes/auth.routes');
+const connectDB = require("./configs/database");
+
+connectDB(process.env.MONGO_URI);
+
 app.use('/auth', spotifyAuthRoutes);
 
 const PORT = process.env.PORT || 5003;
