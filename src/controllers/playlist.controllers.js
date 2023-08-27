d9b74c4fa004319bbcfa16bce913c52a040f1efb
David Umoru <umorudavido@gmail.com>
2023-08-27 18:04:55 +0100

    Implement controller for mood based playlist

---
+++ src/controllers/playlist.controllers.js
@@ -0,0 +1,21 @@
+const playlistService = require('../services/playlist.services');
+
+const createMoodBasedPlaylist = async (req, res) => {
+  const { accessToken, userId, mood } = req.body;
+
+  try {
+    const playlistId = await playlistService.createMoodBasedPlaylist(accessToken, userId, mood);
+    if (playlistId) {
+      res.status(201).json({ playlistId });
+    } else {
+      res.status(400).json({ message: 'Failed to create playlist' });
+    }
+  } catch (error) {
+    console.error('Error creating mood-based playlist:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+module.exports = {
+  createMoodBasedPlaylist,
+};
