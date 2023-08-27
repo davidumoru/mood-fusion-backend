6aa57007eae1c3fe1e13beff03d8ef150f7a1301
David Umoru <umorudavido@gmail.com>
2023-08-27 15:11:03 +0100

    Add routes for handling Spotify authentication

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
+
+const spotifyAuthRoutes = require('./routes/auth.routes');
+app.use('/auth', spotifyAuthRoutes);
+
 const PORT = process.env.PORT || 5003;
 
 app.listen(PORT, () => {
