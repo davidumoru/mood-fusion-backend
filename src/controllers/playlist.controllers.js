e18bffd297c2c442fd9d72a6a37242222d782557
David Umoru <umorudavido@gmail.com>
2023-09-04 22:15:52 +0100

    Refactor Code

--- src/controllers/playlist.controllers.js
+++ src/controllers/playlist.controllers.js
@@ -1,23 +1,39 @@
-const playlistService = require('../services/playlist.services');
+const playlistService = require("../services/playlist.services");
 
 const createMoodBasedPlaylist = async (req, res) => {
   const { accessToken, userId, mood } = req.body;
 
   try {
-    const playlistId = await playlistService.createMoodBasedPlaylist(accessToken, userId, mood);
+    const playlistId = await playlistService.createMoodBasedPlaylist(
+      accessToken,
+      userId,
+      mood
+    );
     if (playlistId) {
       res.status(201).json({ playlistId });
     } else {
-      res.status(400).json({ message: 'Failed to create playlist. Please check your inputs.' });
+      res
+        .status(400)
+        .json({
+          message: "Failed to create playlist. Please check your inputs.",
+        });
     }
   } catch (error) {
-    console.error('Error creating mood-based playlist:', error);
-    if (error.message === 'Invalid access token') {
-      res.status(401).json({ message: 'Invalid access token. Please provide a valid access token.' });
-    } else if (error.message === 'Invalid user ID') {
-      res.status(400).json({ message: 'Invalid user ID. Please provide a valid user ID.' });
+    console.error("Error creating mood-based playlist:", error);
+    if (error.message === "Invalid access token") {
+      res
+        .status(401)
+        .json({
+          message: "Invalid access token. Please provide a valid access token.",
+        });
+    } else if (error.message === "Invalid user ID") {
+      res
+        .status(400)
+        .json({ message: "Invalid user ID. Please provide a valid user ID." });
     } else {
-      res.status(500).json({ message: 'Internal server error. Please try again later.' });
+      res
+        .status(500)
+        .json({ message: "Internal server error. Please try again later." });
     }
   }
 };
@@ -26,20 +42,37 @@ const createSongBasedPlaylist = async (req, res) => {
   const { accessToken, userId, inputSongId } = req.body;
 
   try {
-    const playlistId = await playlistService.createSongBasedPlaylist(accessToken, userId, inputSongId);
+    const playlistId = await playlistService.createSongBasedPlaylist(
+      accessToken,
+      userId,
+      inputSongId
+    );
     if (playlistId) {
       res.status(201).json({ playlistId });
     } else {
-      res.status(400).json({ message: 'Failed to create song-based playlist. Please check your inputs.' });
+      res
+        .status(400)
+        .json({
+          message:
+            "Failed to create song-based playlist. Please check your inputs.",
+        });
     }
   } catch (error) {
-    console.error('Error creating song-based playlist:', error);
-    if (error.message === 'Invalid access token') {
-      res.status(401).json({ message: 'Invalid access token. Please provide a valid access token.' });
-    } else if (error.message === 'Invalid user ID') {
-      res.status(400).json({ message: 'Invalid user ID. Please provide a valid user ID.' });
+    console.error("Error creating song-based playlist:", error);
+    if (error.message === "Invalid access token") {
+      res
+        .status(401)
+        .json({
+          message: "Invalid access token. Please provide a valid access token.",
+        });
+    } else if (error.message === "Invalid user ID") {
+      res
+        .status(400)
+        .json({ message: "Invalid user ID. Please provide a valid user ID." });
     } else {
-      res.status(500).json({ message: 'Internal server error. Please try again later.' });
+      res
+        .status(500)
+        .json({ message: "Internal server error. Please try again later." });
     }
   }
 };
@@ -57,20 +90,35 @@ const createArtistBasedPlaylist = async (req, res) => {
     if (playlistId) {
       res.status(201).json({ playlistId });
     } else {
-      res.status(400).json({ message: 'Failed to create artist-based playlist. Please check your inputs.' });
+      res
+        .status(400)
+        .json({
+          message:
+            "Failed to create artist-based playlist. Please check your inputs.",
+        });
     }
   } catch (error) {
-    console.error('Error creating artist-based playlist:', error);
-    if (error.message === 'Invalid access token') {
-      res.status(401).json({ message: 'Invalid access token. Please provide a valid access token.' });
-    } else if (error.message === 'Invalid user ID') {
-      res.status(400).json({ message: 'Invalid user ID. Please provide a valid user ID.' });
+    console.error("Error creating artist-based playlist:", error);
+    if (error.message === "Invalid access token") {
+      res
+        .status(401)
+        .json({
+          message: "Invalid access token. Please provide a valid access token.",
+        });
+    } else if (error.message === "Invalid user ID") {
+      res
+        .status(400)
+        .json({ message: "Invalid user ID. Please provide a valid user ID." });
     } else {
-      res.status(500).json({ message: 'Internal server error. Please try again later.' });
+      res
+        .status(500)
+        .json({ message: "Internal server error. Please try again later." });
     }
   }
 };
 
 module.exports = {
-  createMoodBasedPlaylist, createSongBasedPlaylist, createArtistBasedPlaylist
+  createMoodBasedPlaylist,
+  createSongBasedPlaylist,
+  createArtistBasedPlaylist,
 };
