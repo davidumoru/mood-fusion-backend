237ff56792ed95645dcbac65a1857c44dcd79d02
David Umoru <umorudavido@gmail.com>
2023-09-04 19:00:43 +0100

    Implement song based playlist creation

--- src/services/playlist.services.js
+++ src/services/playlist.services.js
@@ -74,6 +74,106 @@ const createMoodBasedPlaylist = async (accessToken, userId, mood) => {
   }
 };
 
+const createSongBasedPlaylist = async (accessToken, inputSongId) => {
+  try {
+    // Get the audio features of the input song
+    const audioFeaturesResponse = await axios.get(
+      `https://api.spotify.com/v1/audio-features/${inputSongId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    // Check if the response contains the expected data
+    if (!audioFeaturesResponse.data || !audioFeaturesResponse.data.id) {
+      throw new Error("Invalid audio features response");
+    }
+
+    const inputAudioFeatures = audioFeaturesResponse.data;
+
+    // Find similar songs based on audio features
+    const similarTracksResponse = await axios.get(
+      "https://api.spotify.com/v1/recommendations",
+      {
+        params: {
+          seed_tracks: inputSongId,
+          target_danceability: inputAudioFeatures.danceability,
+          target_tempo: inputAudioFeatures.tempo,
+          // You can adjust other parameters based on your preference
+        },
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    // Check if the response contains the expected data
+    if (!similarTracksResponse.data || !similarTracksResponse.data.tracks) {
+      throw new Error("Invalid similar tracks response");
+    }
+
+    const similarTracks = similarTracksResponse.data.tracks.map(
+      (track) => track.uri
+    );
+
+    // Create a playlist with the name of the input song and description
+    const playlistResponse = await axios.post(
+      `https://api.spotify.com/v1/me/playlists`,
+      {
+        name: `Song-Based Playlist: ${inputSongId}`,
+        description: `Playlist based on the song ${inputSongId} and its similar tracks`,
+        public: false,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    // Check if the response contains the expected data
+    if (!playlistResponse.data || !playlistResponse.data.id) {
+      throw new Error("Invalid playlist response");
+    }
+
+    const playlistId = playlistResponse.data.id;
+
+    // Add the similar tracks to the playlist
+    const addTracksResponse = await axios.post(
+      `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+      {
+        uris: similarTracks,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    // Check if the response contains the expected data
+    if (!addTracksResponse.data || !addTracksResponse.data.snapshot_id) {
+      throw new Error("Invalid add tracks response");
+    }
+
+    return playlistId;
+  } catch (error) {
+    console.error("Failed to create song-based playlist:", error.message);
+    return {
+      success: false,
+      message: "Failed to create song-based playlist",
+      error: error.message,
+    };
+  }
+};
+
 module.exports = {
   createMoodBasedPlaylist,
+  createSongBasedPlaylist,
 };
